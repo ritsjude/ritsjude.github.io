e55d3823d93b1ec1d61c7d51c48a0c58eee2e243
jokerhunter <jiaoya@example.com>
2019-10-30 15:43:13 +0800

    initial commit

---
+++ precache-manifest.f107e3072c9d0131a8c31a47babe5837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d09c230a54efa0786a5450bc5d1a510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe37e8b10daf6cbee63a",
+    "url": "/static/css/2.6041caac.chunk.css"
+  },
+  {
+    "revision": "e7936ae41dfa7cd13331",
+    "url": "/static/css/main.a94dd50f.chunk.css"
+  },
+  {
+    "revision": "fe37e8b10daf6cbee63a",
+    "url": "/static/js/2.f342fe05.chunk.js"
+  },
+  {
+    "revision": "e7936ae41dfa7cd13331",
+    "url": "/static/js/main.8afaf770.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79e6298ee5b810596b9d0b06d14b1edf",
+    "url": "/static/media/ricoh_logo.79e6298e.png"
+  }
+]);
