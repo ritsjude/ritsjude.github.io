36166194efcdffce5229e2d312bf764af308f59f
jokerhunter <jiaoya@example.com>
2019-10-30 16:42:47 +0800

    add 404.html and appid

---
+++ precache-manifest.493e900525558bb8dc8e480ce0115a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c09348f7a77cd8f6c1474dcab70d500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe37e8b10daf6cbee63a",
+    "url": "/static/css/2.6041caac.chunk.css"
+  },
+  {
+    "revision": "8d327d3aa84afa2b64da",
+    "url": "/static/css/main.a94dd50f.chunk.css"
+  },
+  {
+    "revision": "fe37e8b10daf6cbee63a",
+    "url": "/static/js/2.f342fe05.chunk.js"
+  },
+  {
+    "revision": "8d327d3aa84afa2b64da",
+    "url": "/static/js/main.f5feaeb7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79e6298ee5b810596b9d0b06d14b1edf",
+    "url": "/static/media/ricoh_logo.79e6298e.png"
+  }
+]);
